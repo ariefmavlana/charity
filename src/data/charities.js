781e8bbef79bacb85a5150357c4477d4df9709fa
ariefmavlana <ariefmaulana0808@gmail.com>
2023-11-01 14:55:55 +0700

    Initial Commit II

---
+++ src/data/charities.js
@@ -0,0 +1,16 @@
+module.exports = {
+    charities: [
+        {
+            id: 1,
+            title: 'First Company Project',
+            category: 'Money',
+            thumbnail: 'https://live.staticflickr.com/65535/17121706878_aed6a3421c_t.jpg',
+            pledged: 155515,
+            target: 1000000,
+            date_end: +new Date('10 August 2023'),
+            profile_photo: 'https://live.staticflickr.com/4027/4357722810_7136f4a9e9_s.jpg',
+            profile_name: 'Vioride Bueno',
+            no_pledges: 0,
+        }
+    ]
+}
