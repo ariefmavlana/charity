5c951520d9859b48439f0d46ed7d6fa92112c1b6
ariefmavlana <ariefmaulana0808@gmail.com>
2023-11-01 14:58:49 +0700

    Initial Commit II

--- src/data/charities.js
+++ src/data/charities.js
@@ -5,7 +5,7 @@ module.exports = {
             title: 'First Company Project',
             category: 'Money',
             thumbnail: 'https://live.staticflickr.com/65535/17121706878_aed6a3421c_t.jpg',
-            pledged: 155515,
+            pledged: 19550,
             target: 1000000,
             date_end: +new Date('10 August 2023'),
             profile_photo: 'https://live.staticflickr.com/4027/4357722810_7136f4a9e9_s.jpg',
