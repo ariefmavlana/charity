b6e28c9f8575ce7bf58bea2862eaf5ee8637f8ec
ariefmavlana <ariefmaulana0808@gmail.com>
2023-11-08 11:11:34 +0700

    Innitial Commit

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -4,8 +4,8 @@
     <div class="xs-footer-top-layer">
       <div class="row">
         <div class="col-lg-3 col-md-6 footer-widget xs-pr-20">
-          <a href="index.html" class="xs-footer-logo">
-            <img src="assets/images/footer_logo.png" alt="" />
+          <a href="/" class="xs-footer-logo">
+            <img src="/assets/images/footer_logo.png" alt="" />
           </a>
           <p>
             CharityPress online and raise money for charity and causes you’re
@@ -14,18 +14,24 @@
           </p>
           <ul class="xs-social-list-v2">
             <li>
-              <a href="" class="color-facebook"><i class="fa fa-facebook" /></a>
+              <a href="" class="color-facebook">
+                <i class="fa fa-facebook" />
+              </a>
             </li>
             <li>
-              <a href="" class="color-twitter"><i class="fa fa-twitter" /></a>
+              <a href="" class="color-twitter">
+                <i class="fa fa-twitter" />
+              </a>
             </li>
             <li>
-              <a href="" class="color-dribbble"><i class="fa fa-dribbble" /></a>
+              <a href="" class="color-dribbble">
+                <i class="fa fa-dribbble" />
+              </a>
             </li>
             <li>
-              <a href="" class="color-pinterest"
-                ><i class="fa fa-pinterest" /></a
-              >
+              <a href="" class="color-pinterest">
+                <i class="fa fa-pinterest" />
+              </a>
             </li>
           </ul>
           <!-- .xs-social-list END -->
@@ -33,29 +39,40 @@
         <div class="col-lg-4 col-md-6 footer-widget">
           <h3 class="widget-title">About Us</h3>
           <ul class="xs-footer-list">
-            <li><a href="index.html">About employee</a></li>
-            <li><a href="#">How it works</a></li>
-            <li><a href="#">Careers</a></li>
-            <li><a href="#">Press</a></li>
-            <li><a href="#">Blog</a></li>
-            <li><a href="#">Contact</a></li>
+            <li>
+              <a href="/">About employee</a>
+            </li>
+            <li>
+              <a href="#">How it works</a>
+            </li>
+            <li>
+              <a href="#">Careers</a>
+            </li>
+            <li>
+              <a href="#">Press</a>
+            </li>
+            <li>
+              <a href="#">Blog</a>
+            </li>
+            <li>
+              <a href="/contact">Contact</a>
+            </li>
           </ul>
         </div>
         <div class="col-lg-4 col-md-6 footer-widget">
           <h3 class="widget-title">Contact Us</h3>
           <ul class="xs-info-list">
             <li>
-              <i class="fa fa-home" />Sector # 48, 123 Street, miosya road VIC
-              28, Australia.
+              <i class="fa fa-home" />
+              Sector # 48, 123 Street, miosya road VIC 28, Australia.
             </li>
             <li>
-              <i class="fa fa-phone" />(800) 123.456.7890 (800) 123.456.7890 +00
-              99 88 5647
+              <i class="fa fa-phone" />
+              (800) 123.456.7890 (800) 123.456.7890 +00 99 88 5647
             </li>
             <li>
-              <i class="fa fa-envelope-o" /><a href="mailto:yourname@domain.com"
-                >yourname@domain.com</a
-              >
+              <i class="fa fa-envelope-o" />
+              <a href="mailto:yourname@domain.com">yourname@domain.com</a>
             </li>
           </ul>
           <!-- .xs-list-with-icon END -->
@@ -74,9 +91,15 @@
         <div class="col-md-6">
           <nav class="xs-footer-menu">
             <ul>
-              <li><a href="#">FAQ</a></li>
-              <li><a href="#">Help Desk</a></li>
-              <li><a href="#">Support</a></li>
+              <li>
+                <a href="#">FAQ</a>
+              </li>
+              <li>
+                <a href="#">Help Desk</a>
+              </li>
+              <li>
+                <a href="#">Support</a>
+              </li>
             </ul>
           </nav>
         </div>
@@ -84,6 +107,8 @@
     </div>
   </div>
   <div class="xs-back-to-top-wraper">
-    <a href="#" class="xs-back-to-top"><i class="fa fa-angle-up" /></a>
+    <a href="#" class="xs-back-to-top">
+      <i class="fa fa-angle-up" />
+    </a>
   </div>
 </footer>
